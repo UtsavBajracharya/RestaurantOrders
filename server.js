60c6a55ac82abd0c4275a937f35b821d1958a019
Utsav <utsavbajra619@gmail.com>
2025-01-23 23:53:20 -0500

    Remove unwanted schema

--- server.js
+++ server.js
@@ -26,18 +26,6 @@ mongoose
     process.exit(1);
   });
 
-const orderSchema = new mongoose.Schema({
-  orderId: { type: String, unique: true, required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  quantity: { type: Number, required: true },
-  price: { type: Number, required: true },
-  foodOrdered: { type: String, required: true },
-  date: { type: Date, default: Date.now },
-});
-
-const Order = mongoose.model('Order', orderSchema);
-
 // Create a new order
 app.post('/orders', async (req, res) => {
   if (!req.body || Object.keys(req.body).length === 0) {
